78c611e40916ef7fc8ee6ab209b9b0618e80fcb8
Ricardo Presilla <ricardopresilla@gmail.com>
2021-11-29 10:59:57 -0400

    Agregado archivo
    Agregado archivo con el método main.

---
+++ src/abstracFactory/AbstractFactory.java
@@ -0,0 +1,27 @@
+package abstractFactory;
+/**
+ * Implementa el método main para la ejecución del programa. Ejemplo de aplicación del patroncreacional Abstract Factory.
+ * @author Ricardo Presilla.
+ * @version 1.0.
+ */
+public class AbstractFactory {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SamsungStore samsung = new SamsungStore();
+        AppleStore apple = new AppleStore();
+
+        IComputadora mac = apple.crearComputadora();
+        ITablet ipad = apple.crearTablet();
+        IPhone smartPhone = apple.crearMovil();
+
+        IComputadora pc = samsung.crearComputadora();
+        ITablet tabletSamsung = samsung.crearTablet();
+        IPhone galaxy = samsung.crearMovil();
+
+        System.out.println("Ejemplo de aplicación del patron creacional Abstract Factory.");
+        System.out.println(apple.mensaje());
+        System.out.println(samsung.mensaje());
+    }
+}
